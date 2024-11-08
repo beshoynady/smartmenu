85e88a5e98ce8420e5cdd7979e6723613a6e8e05
beshoynady <bishoin2017@gmail.com>
2024-11-08 10:01:43 +0200

    btn

--- client/src/screens/management/manag.component/products/MenuCategory.jsx
+++ client/src/screens/management/manag.component/products/MenuCategory.jsx
@@ -521,8 +521,8 @@ const MenuCategory = () => {
                         ))}
                       </div>
                       <div className="modal-footer flex-nowrap d-flex flex-row align-items-center justify-content-between">
-                        <input type="submit" className="btn btn-success col-md-6 col-12 h-100 p-0 m-0 " value="حفظ" />
-                        <input type="button" className="btn btn-danger col-md-6 col-12 h-100 p-0 m-0 " data-dismiss="modal" value="إغلاق" />
+                        <input type="submit" className="btn btn-success col-6 h-100 px-2 py-3 m-0" value="حفظ" />
+                        <input type="button" className="btn btn-danger col-6 h-100 px-2 py-3 m-0" data-dismiss="modal" value="إغلاق" />
                       </div>
                     </form>
                   </div>
@@ -541,7 +541,7 @@ const MenuCategory = () => {
                         <p className="text-warning text-center mt-3"><small>لا يمكن الرجوع فيه.</small></p>
                       </div>
                       <div className="modal-footer d-flex flex-nowrap align-items-center justify-content-between m-0 p-1">
-                        <input type="submit" className="btn btn-warning  col-6 h-100 px-2 py-3 m-0" value="حذف" />
+                        <input type="submit" className="btn btn-warning col-6 h-100 px-2 py-3 m-0" value="حذف" />
                         <input type="button" className="btn btn-danger col-6 h-100 px-2 py-3 m-0" data-dismiss="modal" value="إغلاق" />
                       </div>
                     </form>
