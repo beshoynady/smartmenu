0f9e1fe1b756a5fe7c7294460344ed48ef67c975
beshoynady <bishoin2017@gmail.com>
2024-11-26 21:51:03 +0200

    Grill

--- client/src/screens/management/manag.component/kitchen/Grill.jsx
+++ client/src/screens/management/manag.component/kitchen/Grill.jsx
@@ -701,7 +701,7 @@ const Grill = () => {
                     </div>
                     <ul className="list-group list-group-flush">
                       {order.products
-                        .filter((product) => product.isDone === false && product.productid?.preparationSection ==='Bar')
+                        .filter((product) => product.isDone === false && product.productid?.preparationSection ==='Grill')
                         .map((product, i) => {
                           return (
                             <>
